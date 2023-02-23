ce387a7d9c40c1c26a963b4ceaaaae6f5e144f0d
gytrbn <101428208+gytrbn@users.noreply.github.com>
2023-02-23 09:04:40 +0200

    pirmos uzduoties uzbaigimas
    Ekrane rodomas text tipo input laukelis ir submit mygtukas formai patvirtinti. Įvedus skaičių ir patvirtinus formą sistema turi atspausdinti lentelę su tiek eilučių koks buvo įvestas skaičius.
    Užduoties įgyvendinimui turi pakakti panaudoti vieną for cikla
    
    Papildomas užduotis:
    Kas antra lentelė eilutė turi būti nuspalvinta kažkokia pasirinkta spalva
    Papildomos užduoties įgyvendinimui reikės panaudoti if sąlygą kurioje reikes patikrinti ar eilutė yra lyginio skaičiaus ar ne. Galima panaudoti mod funkciją https://www.w3schools.com/php/phptryit.asp?filename=tryphp_oper_modulus Jei $i % 2 == 0 skaicius lyginis, jei $i % 2 == 1 vadinasi skaicius nelyginis
    
    Papildoma užduotis aukštesnis lygis:
    patikrinti ar $_GET kinamajame egzsituoja prametras pavadinimu skaicius ir patikrinti ar įvesti duomenys yra integer tipo. Lentelės piešimo logiką perkelti į funkciją. Į funkciją reikia paduoti skaiciu o funkcija išspausdina lentelę

--- index.php
+++ index.php
@@ -14,24 +14,39 @@ td{
 <body>
 
 <form method="get">
-<input type="text" name="skaicius" value="0"><br>
+<input type="text" name="skaicius"><br>
 <input type="submit" value="Submit"><br>
 </form>
 
 <table>
 <?php
 
-$num=0;
-
-
+$num="";
 
 if (isset($_GET["skaicius"])){
 	$num=$_GET["skaicius"];
 }
 
+if (is_numeric($num)){
+    lentele((int)$num);
+}else {
+    echo "Iveskite skaiciu";
+}
 
-for ($i=0;$i<$num;$i++){
-	echo "<tr><td>tekstas</td></tr>";
+
+function lentele($x){
+
+    $bgcl="background-color:grey;";
+
+    for ($i=0;$i<$x;$i++){
+
+    if ($i%2==0){
+        echo "<tr><td>tekstas</td></tr>";
+     }else if ($i%2==1){
+	    echo "<tr><td style='$bgcl'>tekstas</td></tr>";
+    }else echo "Klaida!";
+    }
+
 }
 ?>
 </table>
