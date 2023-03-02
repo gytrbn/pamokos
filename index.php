0ef450c93a8679192e215691eb0c552ac0a50fc6
gytrbn <101428208+gytrbn@users.noreply.github.com>
2023-03-02 10:59:07 +0200

    padarytas masyvas ir pradzia paieskos

--- index.php
+++ index.php
@@ -1,4 +1,25 @@
+<form method="get">
+<input type="text" name="search">
+<input type="submit">
+</form>
+
 <?php
 
+$cars = array(
+    array('Mercedes', 'S500', 'E280', 'Sprinter', 'C63', 'Vito', 'ML350'),
+    array('Nissan', 'Figaro', 'Navara', 'Leaf', 'Silvia'),
+    array('Skoda', 'Octavia', 'Superb', 'Fabia'),
+    array('Volkswagen', 'Passat', 'Golf','Phaeton', 'Touran','Polo'),
+    array('BMW', '328', '116', '520', '323', '750i')
+);
+
+if (isset($_GET['search'])){
+    $searchText = $_GET['search'];
+    if (array_key_exists($searchText, $cars)){
+        echo 'yra toks';
+    }else{
+        echo 'Nera';
+    }
+}
 
 ?>
